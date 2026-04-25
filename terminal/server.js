a00c9f8fcf9305af28ff8288c8592d3719b512ed
ww5103543-boop <ww5103543@gmail.com>
2026-04-25 15:12:24 -0700

    Update server.js

--- terminal/server.js
+++ terminal/server.js
@@ -5,7 +5,7 @@ const os = require('os');
 const path = require('path');
 
 const app = express();
-const PORT = 3001;  // Different port
+const PORT = 3001;
 
 app.use(express.static(__dirname));
 
@@ -21,13 +21,20 @@ const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws) => {
   let currentDir = os.homedir();
+  console.log('Client connected');
+  
   const shell = spawn('/bin/bash', ['-l'], {
     cwd: currentDir,
     env: { ...process.env, TERM: 'xterm-256color', PATH: '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' }
   });
   
-  shell.stdout.on('data', (data) => ws.send(JSON.stringify({ type: 'output', data: data.toString() })));
-  shell.stderr.on('data', (data) => ws.send(JSON.stringify({ type: 'output', data: `\x1b[31m${data.toString()}\x1b[0m` })));
+  shell.stdout.on('data', (data) => {
+    ws.send(JSON.stringify({ type: 'output', data: data.toString() }));
+  });
+  
+  shell.stderr.on('data', (data) => {
+    ws.send(JSON.stringify({ type: 'output', data: `\x1b[31m${data.toString()}\x1b[0m` }));
+  });
   
   ws.on('message', (msg) => {
     const cmd = msg.toString();
@@ -35,7 +42,11 @@ wss.on('connection', (ws) => {
       ws.send(JSON.stringify({ type: 'cwd', data: currentDir }));
     } else if (cmd.startsWith('__cd__:')) {
       const newDir = cmd.substring(7).replace('~', os.homedir());
-      try { process.chdir(newDir); currentDir = process.cwd(); ws.send(JSON.stringify({ type: 'cwd', data: currentDir })); } catch(e) {}
+      try {
+        process.chdir(newDir);
+        currentDir = process.cwd();
+        ws.send(JSON.stringify({ type: 'cwd', data: currentDir }));
+      } catch(e) {}
     } else {
       shell.stdin.write(cmd + '\n');
     }
